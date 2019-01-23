e340e5d89e991ebfe58873e2a30006d1f6db0cc8
Kieran Elmes <kieran.elmes@kolabnow.com>
2019-01-24 12:36:16 +1300

    fix ignoreing already processed files, use write setting

--- fit_all_1204.sh
+++ fit_all_1204.sh
@@ -9,11 +9,11 @@ threads=12
 for f in `ls simulated_data`; do
 	for L in 100; do
 		if [[ -f fits_proper/`expr "$f" : '\(.*_\)'`L$L`expr "$f" : '.*\(_.*\)'` ]]; then
+			echo "file '$f' already fitted, ignoring"
+		else
 			((i=i%threads)); ((i++==0)) && wait
 			{ echo "$file '$f' not found, fitting now."
-			./fits_xyz_only.R $f $L || true; } &
-		else
-			echo "file '$f' already fitted, ignoring"
+			./fits_xyz_only.R simulated_data/$f $L write || true; } &
 		fi
 	done
 done
