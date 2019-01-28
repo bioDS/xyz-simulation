28d88a143d9c7fa73e1415894679f5b7931ff1b6
Kieran Elmes <kieran.elmes@kolabnow.com>
2019-01-29 12:24:38 +1300

    neater labels

--- PrecRecF1/PrecRecF1_lethals.R
+++ PrecRecF1/PrecRecF1_lethals.R
@@ -101,10 +101,10 @@ for (numrows in graph_numrows) { #400
           geom_line(aes(colour = nbij), position=position_dodge(.35)) +
           geom_point(aes(colour = nbij), position=position_dodge(.35), size = 1) +
           geom_errorbar(colour = "darkgrey", width=.3, position=position_dodge(.35)) +
-    #      scale_color_discrete(name = "True additional\nmain effects") +
+          scale_color_discrete(name = "True additional interactions") +
           # scale_colour_manual(name = "Additional main effects", values = c("#7fcdbb", "#1d91c0", "#253494")) + #brewer.pal(7, "Set3")[-1]) + #
           ylim(c(0,1)) +
-          #xlab("True interactions") +
+          xlab("True lethal pairs") +
           ylab("") +
           #theme_fs() +
           theme(legend.position = "bottom")
