718aade5d45464a82727fe1d2fbb29be9db4f952
Kieran Elmes <kieran.elmes@kolabnow.com>
2019-02-11 13:15:53 +1300

    update patches for new rpo

--- update_repo.sh
+++ update_repo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for i in `grep -rhoP simulation/[^\\\\s]+\\\\.R 1204_patches/ | sort | uniq`; do
-	git checkout -- "../$i"
+for i in `grep -rhoP [^\\\\s]+\\\\.R 1204_patches/ | sort | uniq`; do
+	git checkout -- "$i"
 done
 git pull
 for i in `ls 1204_patches`; do
-	patch -p2 < 1204_patches/$i
+	patch -p0 < 1204_patches/$i
 done
