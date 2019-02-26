375924f973bdb190b879fab717d9a8e08994e107
Kieran Elmes <kieran.elmes@kolabnow.com>
2019-02-26 16:14:34 +1300

    optional xyz/glinternet for FXdiff

--- FXdiff/FXdiff.R
+++ FXdiff/FXdiff.R
@@ -8,6 +8,18 @@ setwd("..")
 args <- commandArgs(trailingOnly = TRUE)
 mult <- args[1] %>% as.numeric
 
+if (args[2] == "x") {
+       use_xyz = TRUE
+       fits_path='./fits_proper/'
+} else if (args[2] == "g") {
+       use_xyz = FALSE
+       fits_path='./fits_glinternet/'
+} else {
+    cat("[x]yz or [g]linternet required\n")
+    q()
+}
+rds_file = sprintf("FXstrength/dat_fxstrength_xyz%s.rds", use_xyz)
+
 for (numrows in c(1000 * mult)) { #400, 1000
   if (numrows == 400) {
     rseq <- 0#c(seq(0, 60, by = 20), 100)
@@ -19,12 +31,14 @@ for (numrows in c(1000 * mult)) { #400, 1000
     rseq <- mult*c(0, 10, 20, 40, 80, Inf)#c(seq(0, 150, by = 50), 250)
   }
   for (t in c("yes", "no")) {
-    dat_dir <- readRDS("FXstrength/dat_fxstrength.rds") %>%
+    dat_dir <- readRDS(file = rds_file)
+      if (use_xyz)
+        dat_dir <- dat_dir %>% filter(L == round(sqrt(p %>% as.character %>% as.numeric)))
+      dat_dir <- dat_dir %>%
       filter(type == "TP") %>%
       filter(n == numrows) %>%
       filter(SNR != 1) %>%
-      filter(L == round(sqrt(p %>% as.character %>% as.numeric))) %>%
-#      mutate(SNR = factor(SNR, labels = paste0("SNR = ", levels(SNR)))) %>%
+      mutate(SNR = factor(SNR, labels = paste0("SNR = ", levels(SNR)))) %>%
       filter(nbi == 20*mult) %>%
       filter(nbij %in% c(5*mult, 20*mult, 50*mult, 100*mult)) %>%
       filter(test == t) %>%
@@ -55,7 +69,7 @@ for (numrows in c(1000 * mult)) { #400, 1000
       theme(legend.position = "bottom",
             axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
     pl
-    ggsave(pl, file = sprintf("FXdiff/FXdiff_n%d_t%s.pdf", numrows, t), width = 5, height = 4)
+    ggsave(pl, file = sprintf("FXdiff/FXdiff_n%d_t%s_xyz%s.pdf", numrows, t, use_xyz), width = 5, height = 4)
   }
 }
 
