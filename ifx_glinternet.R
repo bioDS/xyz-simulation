58faa6d6d64865e931ca48c216a5dfa8d10a2893
Kieran Elmes <=>
2019-10-08 10:15:11 +1300

    exclude all-zero columns

--- ifx_glinternet.R
+++ ifx_glinternet.R
@@ -13,6 +13,7 @@ cl_bartonella = cl_output$bartonella
 filtered_cl_bartonella = cl_bartonella %>% select(Catalog_number, eCount_oCells) %>% filter(!is.na(eCount_oCells))
 
 X = ifx[filtered_cl_bartonella$Catalog_number,]
+X = X[,colSums(X) != 0]
 Y = log2(filtered_cl_bartonella$eCount_oCells / mean(filtered_cl_bartonella$eCount_oCells))
 
 gl_output = glinternet.cv(X, Y, numLevels=rep(1,dim(X)[2]))
