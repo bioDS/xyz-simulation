ca62804e53181fb814193256073d269d26a0942c
Kieran Elmes <=>
2019-10-08 09:59:50 +1300

    only use bartonella & kinome, save output

--- ifx_glinternet.R
+++ ifx_glinternet.R
@@ -6,13 +6,15 @@ library(Matrix)
 
 ifx = readRDS("./IFX_QIAGEN.rds")
 
-#cl_output = clean()
-cl_output = readRDS("./cl_output.rds")
+cl_output = clean("bartonella", cases = c("Kinome"), libraries = c("Qiagen"))
+#cl_output = readRDS("./cl_output.rds")
 
-cl_adeno = cl_output$adeno
-filtered_cl_adeno = cl_adeno %>% filter(LIBRARY=="Qiagen") %>% select(Catalog_number, eCount_oCells) %>% filter(!is.na(eCount_oCells))
+cl_bartonella = cl_output$bartonella
+filtered_cl_bartonella = cl_bartonella %>% select(Catalog_number, eCount_oCells) %>% filter(!is.na(eCount_oCells))
 
-X = ifx[filtered_cl_adeno$Catalog_number,]
-Y = log2(filtered_cl_adeno$eCount_oCells / mean(filtered_cl_adeno$eCount_oCells))
+X = ifx[filtered_cl_bartonella$Catalog_number,]
+Y = log2(filtered_cl_bartonella$eCount_oCells / mean(filtered_cl_bartonella$eCount_oCells))
 
-#gl_output = glinternet.cv(X, Y, numLevels=rep(1,dim(X)[2]))
+gl_output = glinternet.cv(X, Y, numLevels=rep(1,dim(X)[2]))
+
+saveRDS(gl_output, "gl_output.rds")
