4444cb29d0f7c31c4d6ce75f49d3e2af7805d728
Kieran Elmes <kieran.elmes@kolabnow.com>
2019-10-29 10:37:07 +1300

    use diffs from seed cells as fitness, remove duplicate columns

--- ifx_glinternet.R
+++ ifx_glinternet.R
@@ -15,23 +15,33 @@ cl_output = clean(cases = c("Kinome"), libraries = c("Qiagen"))
 kinases = c()
 catalog_numbers = c()
 fitness_measure = c()
+cl_output$adeno$seed_cells = 700
+cl_output$bartonella$seed_cells = 300
+cl_output$brucella$seed_cells = 500
+cl_output$listeria$seed_cells = 600
+cl_output$rhino$seed_cells = 1000
+cl_output$salmonella$seed_cells = 550
+cl_output$shigella$seed_cells = 600
+cl_output$vaccinia$seed_cells = 600
 for (pathogen in cl_output) {
 	kinases = c(kinases, as.character(pathogen$ID))
 	pathogen = pathogen %>% filter(!is.na(eCount_oCells))
 	catalog_numbers = c(catalog_numbers, pathogen$Catalog_number)
-	fitness_measure = c(fitness_measure, pathogen$eCount_oCells)
+	fitness_diff = c(fitness_measure, pathogen$eCount_oCells / pathogen$seed_cells)
 }
 kinases = unique(kinases)
 #catalog_numbers = unique(catalog_numbers)
 
 X = ifx[catalog_numbers, kinases]
 X[X != 0] <- 1
+X = as.matrix(X)
+X = X[,!duplicated(t(X))]
 #X = X[,colSums(X) != 0]
-Y = log2(fitness_measure / mean(fitness_measure))
+Y = log2(fitness_measure / fitness_diff)
 
 saveRDS(X, "X.rds")
 saveRDS(fitness_measure, "fitness_measure.rds")
 saveRDS(Y, "Y")
-gl_output = glinternet.cv(X, fitness_measure, numLevels=rep(1,dim(X)[2]), verbose=TRUE,  numCores=10)
-#
-saveRDS(gl_output, "gl_output.rds")
+#gl_output = glinternet.cv(X, fitness_measure, numLevels=rep(1,dim(X)[2]), verbose=TRUE,  numCores=10)
+##
+#saveRDS(gl_output, "gl_output.rds")
