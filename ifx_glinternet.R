b34671283a162a8bc426f4f49e6cfdd142eb4b0b
Kieran Elmes <kelmes@cs.otago.ac.nz>
2019-10-29 12:23:40 +1300

    more typos

--- ifx_glinternet.R
+++ ifx_glinternet.R
@@ -14,7 +14,7 @@ cl_output = clean(cases = c("Kinome"), libraries = c("Qiagen"))
 
 kinases = c()
 catalog_numbers = c()
-fitness_measure = c()
+fitness_diff = c()
 cl_output$adeno$seed_cells = 700
 cl_output$bartonella$seed_cells = 300
 cl_output$brucella$seed_cells = 500
@@ -27,7 +27,7 @@ for (pathogen in cl_output) {
 	kinases = c(kinases, as.character(pathogen$ID))
 	pathogen = pathogen %>% filter(!is.na(eCount_oCells))
 	catalog_numbers = c(catalog_numbers, pathogen$Catalog_number)
-	fitness_diff = c(fitness_measure, pathogen$eCount_oCells / pathogen$seed_cells)
+	fitness_diff = c(fitness_diff, pathogen$eCount_oCells / pathogen$seed_cells)
 }
 kinases = unique(kinases)
 #catalog_numbers = unique(catalog_numbers)
